76ca97d585912432aebc2fbf610816270111075a
Mitchell-Borders <78919160+Mitchell-Borders@users.noreply.github.com>
2022-01-14 13:35:57 -0600

    working ML algo

---
+++ NBAPlayoffPredictor.py
@@ -0,0 +1,86 @@
+import numpy as np
+import sklearn as sklearn
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from utils.MLUtils import *
+
+
+def main():
+    X, y = get_input_output_split('./Team Stats Per Game.csv')
+
+    model_list = [
+        DecisionTreeClassifier(),
+        SVC(probability=True),
+        GaussianNB(),
+        RandomForestClassifier(),
+        LogisticRegression(max_iter=10000),
+        GradientBoostingClassifier(n_estimators=150, max_depth=3)
+    ]
+
+    current_model = [
+        'DecisionTree',
+        'Support Vector Machine',
+        'GaussianNB',
+        'RandomForestClassifier',
+        'LogisticRegression',
+        'GradientBoostingClassifier'
+    ]
+
+    for i in range(0, len(model_list)):
+        print('\n' + current_model[i])
+        model = model_list[i]
+
+        actual_y = []
+        predicted_y = []
+        y_probas = []
+        # implement 10-fold cross-validation
+        fold_no = 1
+        skf = sklearn.model_selection.StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+        for train_index, test_index in skf.split(X, y):
+            train_x = X[train_index]
+            train_y = y[train_index]
+            test_x = X[test_index]
+            test_y = y[test_index]
+
+            model.fit(train_x, train_y)
+
+            pred = model.predict(test_x)
+
+            y_probability = model.predict_proba(test_x)
+            y_probas.append(y_probability)
+
+            # put values of this fold into the array
+            actual_y.append(test_y)
+
+            # use for calculations
+            predicted_y.append(pred)
+            # pred = np.argmax(pred, axis=1)
+
+            fold_accuracy_score = sklearn.metrics.accuracy_score(test_y, pred)
+            print('\nFold', fold_no, 'score (accuracy):', fold_accuracy_score)
+
+            fold_no += 1
+
+        # calculate data for overall model
+        actual_y = np.concatenate(actual_y)
+        predicted_y = np.concatenate(predicted_y)
+        y_probas = np.concatenate(y_probas)
+        # predicted_y = np.argmax(predicted_y, axis = 1)
+
+        overall_accuracy_score = sklearn.metrics.accuracy_score(actual_y, predicted_y)
+        print('\nOverall score (accuracy):', overall_accuracy_score)
+
+        print(sklearn.metrics.classification_report(actual_y, predicted_y, zero_division=0, digits=5))
+        # plot confusion matrics
+        matrix = sklearn.metrics.confusion_matrix(actual_y, predicted_y)
+        cmPlot = sklearn.metrics.ConfusionMatrixDisplay(matrix)
+        cmPlot.plot(cmap='YlGn')
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
